0b85e64b074361f050edd14b7e0a925e79c40e40
BuchtaTomas <tovele.test@gmail.com>
2026-08-18 09:40:54 +0000

    ci(frontend): another attempt to automatically increase tag and build packages/container

--- src/frontend/src/frontend/main.py
+++ src/frontend/src/frontend/main.py
@@ -79,6 +79,7 @@ def index():
 # Add comment to trigger new tag
 # Another comment to trigger new tag
 # Another comment to trigger new tag
+# Another comment to trigger new tag
 #
 @app.route("/add_session", methods=["POST"])
 def add_session():
